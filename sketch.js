1f7ffab024bc5df79e4e60146db810f55f62b1e0
nkwn02 <nathaniel.kwan@sjsu.edu>
2022-03-20 18:38:43 -0700

    node 2.2
    Added:
    HTML
    CSS
    BACKGROUND
    BUTTON
    FACTS
    TEXT FONT
    (change button font + button click fix + phrase association needed)

--- sketch.js
+++ sketch.js
@@ -1,61 +1,146 @@
-let gender = ['Her', 'His', 'Thier'];
-let name = ['Todd', 'Sara', 'Shaggy', 'LeBron', 'Gandalf', 'Yoda', 'Sammy', 'Tracy', 'Kermit', 'Romeo', 'Onyonkopon'];
+let landmarkNames = ['The Painted Ladies', 'Chinatown', 'Golden Gate Park', 'The Conservatory of Flowers', 'The Ferry Building', 'The Palace of Fine Arts', 'The Castro', 'Pier 39', 'Dolores Park', 'The Golden Gate Bridge', 'Coit Tower', 'Downtown', 'Alcatraz'];
+let facts = ['The Fortune Cookie was invented in SF', 'The name of the SF fog is Karl', 'Levis jeans was invented in SF', 'SF has the oldest Japantown in North America','SF has the oldest Chinatown in North America', 'SF is only 7x7 miles', 'Golden Gate Park is bigger than Central Park in NY', 'SF is home to the oldest restraunt in California']
 
+let img;
+let myFont;
 
-let babyrando;
-let genderrando;
-let namerando;
+let sfrando;
+let factsrando;
 
 let words;
 
-let babies = []
+let landmarks = []
+
+//var arr = [];
+
+//arr = [
+   //{
+  //   pic: landmark_0.jpg,
+    // phrase: 'The Painted Ladies',
+//   },
+//   {
+//     pic: landmark_1.jpg,
+//     phrase: 'Chinatown',
+  // },
+   //{
+     //pic: landmark_2.jpg,
+     //phrase: 'Golden Gate Park',
+  // },
+//   {
+  //   pic: landmark_3.jpg,
+    // phrase: 'The Conservatory of Flowers',
+//   },
+  // {
+    // pic: landmark_4.jpg,
+    // phrase: 'The Ferry Building',
+  // },
+  // {
+    // pic: landmark_5.jpg,
+     //phrase: 'The Palace of Fine Arts',
+  // },
+   //{
+     //pic: landmark_6.jpg,
+     //phrase: 'The Castro',
+  // },
+   //{
+     //pic: landmark_7.jpg,
+     //phrase: 'Pier 39',
+  // },
+   //{
+     //pic: landmark_8.jpg,
+    // phrase: 'Dolores Park',
+  // },
+   //{
+     //pic: landmark_9.jpg,
+    // phrase: 'The Golden Gate Bridge',
+  // },
+  // {
+    // pic: landmark_10.jpg,
+    // phrase: 'Coit Tower',
+  // },
+  // {
+    // pic: landmark_11.jpg,
+    // phrase: 'Downtown',
+  // },
+  // {
+    // pic: landmark_12.jpg,
+    // phrase: 'Alcatraz',
+//   },
+//];
+
+
+
+let button;
+let cnv;
 
 function preload(){
 
- for (let i = 0; i <= 9; i++){
-    babies[i] = loadImage(`assets/baby_${i}.jpg`)
+myFont = loadFont('assets/Foglihten-Regular.otf')
+
+  img = loadImage('wallpapersf.jpg');
+
+ for (let i = 0; i <= 12; i++){
+    landmarks[i] = loadImage(`assets/landmark_${i}.jpg`)
+
  }
+
 
 }
 
 function setup() {
-  createCanvas(600, 400);
-  background(200);
+cnv = createCanvas(800, 600);
+cnv.parent('#canvasDiv');
+image(img, 0,0);
+  //background(200);
+  textFont(myFont);
   intText();
-  console.log(babies[int(random(babies.length))]);
-  console.log(gender[3]);
-  console.log(name.length)
+  textSize(30);
+  console.log(landmarks[int(random(landmarks.length))]);
+  console.log(facts.length);
+
+  //button = createButton('Click to learn a fun fact!');
+  button = select('#factmaker');
+  button.position(290, 500);
+  button.mousePressed(buttonPressed);
+  button.class('factButton');
 }
 
 function draw() {
 }
 
-function mousePressed() {
-  babyrando = int(random(babies.length));
-  genderrando = int(random(gender.length));
-  namerando = int(random(name.length));
-  words = ' ' + gender[genderrando] + ' ' + 'name is' + ' ' + name[namerando] + '.';
-  statement();
+function buttonPressed(){
+  factsrando = int(random(facts.length));
+  words = ' ' + facts[factsrando] + '.';
   console.log(words);
 
-//image(babies[0], width/2, height/2)
-image(babies[babyrando], width*0.42, height*0.3, 100, 130);
 }
+
+
+function mousePressed() {
+  sfrando = int(random(landmarks.length));
+  statement();
+
+//image(babies[0], width/2, height/2)
+image(landmarks[sfrando], width*.33, height*0.27, 290, 230);
+}
+
+
 
 
 function intText() {
-  fill(255);
+  fill(156, 129, 67);
   textAlign(CENTER);
   textSize(20);
-  text('Click to make your baby', width*0.5, height*0.1);
+  text('Click to see where you should visit in SF', width*0.5, height*0.1);
 }
 
 function statement() {
-  background(200);
+  image(img, 0, 0);
+  //background(200);
   intText();
-  fill(255);
+  fill(156, 129, 67);
   textAlign(CENTER);
-  textSize(20);
-  text('This is your baby.', width*0.5, height*0.2);
-  text(words, width*0.5, height*0.75)
+  textSize(25);
+  text('You should go to:', width*0.5, height*0.2);
+  text(words, width*0.5, height*0.75);
 }
